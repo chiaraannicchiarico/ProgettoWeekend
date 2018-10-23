5cf26a0771187bcfa207659263850a41a3561758
chiaraannicchiarico <chiara.annicchiaricopetruzzelli@gmail.com>
2018-10-23 18:51:56 +0200

    Restyling Cookie e Like

---
+++ src/js/weekendProject.js
@@ -0,0 +1,32 @@
+var element = document.getElementById('here');
+
+
+
+function closeCookie () {
+
+document.getElementById('cookie_d').style.display="none"; 
+};
+
+element.addEventListener("click",closeCookie);
+
+
+
+
+
+
+
+
+
+	
+
+    var elements = document.getElementsByClassName('like');
+    for (var i = 0; i < like.lenght; i++) { 
+    	elements[i].addEventListener ('click', function (){
+    	if (this.style.backgroundColor = "white"){
+
+    		this.style.backgroundColor = "green";
+    		} else {
+    		this.style.backgroundColor = "white";
+     }} );	
+
+}
